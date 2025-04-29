a3e672993ea2904d6dad745a0aeb215732564153
w3schools-test <test@w3schools.com>
2025-04-29 17:40:27 +0530

    Initial commit

---
+++ components/main-header.js
@@ -0,0 +1,83 @@
+"use client";
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import NavLink from './nav-link';
+
+export default function MainHeader() {
+    const path = usePathname();
+
+    return (
+        <header id="main-header">
+            <div id="logo">
+                <Link href="/">NextNews</Link>
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink href="/news">News</NavLink>
+                    </li>
+                    <li>
+                        <NavLink href="/archive">Archive</NavLink>
+                    </li>
+                    {/* <li>
+                        <Link href="/archive" className={path.startsWith('/archive') ? 'active' : undefined}>Archive</Link>
+                    </li> */}
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+
+
+
+
+
+
+
+
+
+// import Link from "next/link";
+// import styles from "./main-header.module.css";
+
+// export default function MainHeader() {
+//     return (
+//         <header className={styles.home}>
+//             <nav>
+//                 <ul>
+//                     <li><Link href="/" className={styles.item}>Home</Link></li>
+//                     <li><Link href="/news" className={styles.item}>News</Link></li>
+//                 </ul>
+//             </nav>
+//         </header>
+//     );
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import Link from "next/link";
+
+// export default function MainHeader() {
+//     return (
+//         <header>
+//             <ul>
+//                 <li><Link href="/">Home</Link></li>
+//                 <li><Link href="/news">News</Link></li>
+//             </ul>
+//         </header>
+//     )
+// }
